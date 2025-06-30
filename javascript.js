a08e4037c30b0fcbe323d38912976ab3bb69c6c8
svv98 <76674639+svv98@users.noreply.github.com>
2025-06-30 12:36:54 -0700

    Finished Console Game JS, added basic HTML and CSS.

--- javascript.js
+++ javascript.js
@@ -1,22 +1,99 @@
+function gameController(playerOne = 'hi', playerTwo = 'bye', tokenOne = 'X', tokenTwo = 'O'){
+  let board = gameBoard.getBoard();
+  gameBoard.printBoard();
+
+  function player(name, token){
+    name; 
+    token;
+    return {name, token}
+  }
+  const player1 = player(playerOne, tokenOne);
+  const player2 = player(playerTwo, tokenTwo);
+
+  let currentPlayer = player1;
+  let counter=0;
+  const round = (row, col) => {
+    console.log(currentPlayer.name);
+        
+    let change = gameBoard.checkArr(row, col, currentPlayer);
+
+    if(change === true){
+      changePlayer();
+      if(roundCounter() === 9){
+        gameOver();
+      }
+    }
+
+    let win = winCheck();
+    if(win){
+      gameOver(win.name);
+    }
+
+  }; 
+
+  const changePlayer = () => {
+    currentPlayer = (currentPlayer==player1)? player2 : player1;
+    return currentPlayer
+  };
+
+  const winCheck = () => {
+    let winningChances = [
+      board[0][0]+board[0][1]+board[0][2], board[1][0]+board[1][1]+board[1][2], board[2][0]+board[2][1]+board[2][2], 
+      board[0][0]+board[1][0]+board[2][0], board[0][1]+board[1][1]+board[2][1], board[0][2]+board[1][2]+board[2][2],
+      board[0][0]+board[1][1]+board[2][2], board[0][2]+board[1][1]+board[2][0]
+    ];
+    for (let chance of winningChances){
+      if(chance === 'XXX'){
+        return player1
+      }
+      else if (chance === 'OOO'){
+        return player2
+      }
+    }
+  };
+
+  const roundCounter = ()=>{
+    counter++;
+    console.log(counter);
+    return counter
+  };
+
+  return {round}
+};
+
 const gameBoard = (function(){
-  let board= [['_','_','_'],['_','_','_'],['_','_','_']];
-  let getBoard = () => board;
-  return{getBoard}
+  let board= [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']];
+
+  const getBoard = () => board;
+
+  const printBoard = () => {
+    console.log(board[0]);
+    console.log(board[1]);
+    console.log(board[2]);
+  };
+
+  function checkArr(row, col, currentPlayer){
+    if (board[row][col] == ' ') {
+      board[row][col]=currentPlayer.token;
+      printBoard();
+      return true
+    }
+    else {
+      console.log('Token already there');
+      return false
+    };
+  };
+
+  return{getBoard, printBoard, checkArr}
 })();
-gameBoard.getBoard()[0][0] = 'x';
-console.log(gameBoard.getBoard());
 
+function gameOver(winner){
+  if(winner){
+    alert('GAME OVER \n CONGRATS ' + winner.toUpperCase() + '!!!');
+  }
+  else {
+    alert("GAME OVER \n IT'S A TIE!");
+  }
+};
 
-const gameController = (function(){
-
-})();
-
-function players(){
-  let player1 = 1;
-  let player2 = 2;
-  return{player1, player2}
-}
-let player = players();
-console.log(player);
-
-
+let game = gameController();
